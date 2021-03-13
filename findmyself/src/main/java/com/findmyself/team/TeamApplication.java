ed4a3c5a143a41b163c4e875a52416f4b5a01600
haneulhaneul <tmajvm7960@naver.com>
2021-03-13 16:10:07 +0900

    hihihi

--- findmyself/src/main/java/com/findmyself/team/TeamApplication.java
+++ findmyself/src/main/java/com/findmyself/team/TeamApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TeamApplication {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("hibhye");
         SpringApplication.run(TeamApplication.class, args);
     }
 

97a56c588998ea5a116f5b867d0dde9d14dffe97
daeun <sung980524@naver.com>
2021-03-13 16:06:05 +0900

    ??

---
+++ findmyself/src/main/java/com/findmyself/team/TeamApplication.java
@@ -0,0 +1,14 @@
+package com.findmyself.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+        SpringApplication.run(TeamApplication.class, args);
+    }
+
+}

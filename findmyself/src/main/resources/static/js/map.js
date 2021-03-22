21696f81b138fa1c30651a595a6154fd850da5d1
daeun <sung980524@naver.com>
2021-03-22 10:37:37 +0900

    출책

---
+++ findmyself/src/main/resources/static/js/map.js
@@ -0,0 +1,7 @@
+var container = document.getElementById('map');
+var options = {
+    center: new kakao.maps.LatLng(37.566826, 126.9786567),
+    level: 8
+};
+
+var map = new kakao.maps.Map(container, options);

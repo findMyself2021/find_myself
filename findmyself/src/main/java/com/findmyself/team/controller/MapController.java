5b6c7ec251307d64b4eb520dd23bb16154097ac0
daeun <sung980524@naver.com>
2021-03-24 09:28:06 +0900

    강조된 행정동 클릭시 분석 화면으로

--- findmyself/src/main/java/com/findmyself/team/controller/MapController.java
+++ findmyself/src/main/java/com/findmyself/team/controller/MapController.java
@@ -1,20 +1,34 @@
 package com.findmyself.team.controller;
 
+import com.findmyself.team.data.domain.Gudong;
+import com.findmyself.team.data.service.GudongService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequiredArgsConstructor
 public class MapController {
 
+    private final GudongService gudongService;
+
     @PostMapping(value = "/mapAnalysis")
-    public String Analysis(HttpServletRequest httpServletRequest){
+    public String Analysis(HttpServletRequest httpServletRequest, Model model){
 
         String s_hcode = httpServletRequest.getParameter("hcode");
-        long hcode = Long.parseLong(s_hcode);
+        long h_code = Long.parseLong(s_hcode);
 
-        System.out.println(hcode);
+        //행정구
+        final String gu = gudongService.findOne(h_code).getGu();
+        //행정동
+        final String h_dong = gudongService.findOne(h_code).getH_dong();
+
+        model.addAttribute("hcode",h_code);
+        model.addAttribute("gu",gu);
+        model.addAttribute("hdong",h_dong);
 
         return "analysis";
     }

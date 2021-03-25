c41085bf04a3d53011c0a3365e2766b2041d4906
daeun <sung980524@naver.com>
2021-03-25 20:21:34 +0900

    aa

---
+++ findmyself/src/main/resources/static/js/slider.js
@@ -0,0 +1,55 @@
+function sliderStart(){
+    const sliders = document.querySelectorAll(".slider-ui");
+    const slider_deposit = document.querySelectorAll(".slider-deposit");
+
+    sliders.forEach(slider => {
+        let input = slider.querySelector("input[type=range]");
+        let min = input.getAttribute("min");
+        let max = input.getAttribute("max");
+        let valueElem = slider.querySelector(".value");
+
+        slider.querySelector(".min").innerText = min;
+        slider.querySelector(".max").innerText = max;
+
+        function setValueElem() {
+            valueElem.innerText = input.value;
+            let percent = (input.value - min) / (max - min) * 100;
+            valueElem.style.left = percent + "%";
+        }
+        setValueElem();
+
+        input.addEventListener("input", setValueElem);
+        input.addEventListener("mousedown", () => {
+            valueElem.classList.add("up");
+        });
+        input.addEventListener("mouseup", () => {
+            valueElem.classList.remove("up");
+        });
+    });
+
+    slider_deposit.forEach(slider => {
+        let input = slider.querySelector("input[type=range]");
+        let min = input.getAttribute("min");
+        let max = input.getAttribute("max");
+        let valueElem = slider.querySelector(".value");
+
+        slider.querySelector(".min").innerText = min;
+        slider.querySelector(".max").innerText = max;
+
+        function setValueElem() {
+            valueElem.innerText = input.value;
+            let percent = (input.value - min) / (max - min) * 100;
+            valueElem.style.left = percent + "%";
+        }
+        setValueElem();
+
+        input.addEventListener("input", setValueElem);
+        input.addEventListener("mousedown", () => {
+            valueElem.classList.add("up");
+        });
+        input.addEventListener("mouseup", () => {
+            valueElem.classList.remove("up");
+        });
+    });
+}
+

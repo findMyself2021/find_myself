c442dc2c90a94a25f935633dd0128e8f875eb451
SnowFlower97 <gedfgrdt@naver.com>
2021-03-29 22:39:46 +0900

    대중교통 길찾기 경로 analysis에 추가

--- findmyself/src/main/resources/static/js/traffic.js
+++ findmyself/src/main/resources/static/js/traffic.js
@@ -23,63 +23,67 @@ function searchSubwayStationAJAX() {
         }
     }
 }
-
-function searchRootAJAX() {
+function searchPubTransRoot(sx, sy, ex, ey) {
 
     // 출발 위치
-    var sx = 126.93737555322481;
-    var sy = 37.55525165729346;
+    var sx1 = 126.93737555322481;
+    var sy1 = 37.55525165729346;
     // 도착 위치
-    var ex = 126.88265238619182;
-    var ey = 37.481440035175375;
+    var ex1 = 126.88265238619182;
+    var ey1 = 37.481440035175375;
 
-    function searchPubTransPathAJAX() {
-        var xhr = new XMLHttpRequest();
-        //ODsay apiKey 입력
-        var url = "https://api.odsay.com/v1/api/searchPubTransPath?apiKey=VYYJtQrZq5ere3U%2BvOoPhLmqgvRTrFzcpLKrRaKvpcQ&SX="+sx+"&SY="+sy+"&EX="+ex+"&EY="+ey+"&apiKey={YOUR_API_KEY}";
-        xhr.open("GET", url, true);
-        xhr.send();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                console.log( JSON.parse(xhr.responseText) ); // <- xhr.responseText 로 결과를 가져올 수 있음
-                //노선그래픽 데이터 호출
-                callMapObjApiAJAX((JSON.parse(xhr.responseText))["result"]["path"][0].info.mapObj);
-            }
-        }
-    }
-
-    //길찾기 API 호출
-    searchPubTransPathAJAX();
-
-    function callMapObjApiAJAX(mabObj){
-        var xhr = new XMLHttpRequest();
-        //ODsay apiKey 입력
-        var url = "https://api.odsay.com/v1/api/loadLane?apiKey=VYYJtQrZq5ere3U%2BvOoPhLmqgvRTrFzcpLKrRaKvpcQ&mapObject=0:0@"+mabObj+"&apiKey={YOUR_API_KEY}";
-        xhr.open("GET", url, true);
-        xhr.send();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                var resultJsonData = JSON.parse(xhr.responseText);
-                drawKakaoMarker(sx,sy);					// 출발지 마커 표시
-                drawKakaoMarker(ex,ey);					// 도착지 마커 표시
-                drawKakaoPolyLine(resultJsonData);		// 노선그래픽데이터 지도위 표시
-                // boundary 데이터가 있을경우, 해당 boundary로 지도이동
-                if(resultJsonData.result.boundary){
-                    var boundary = new kakao.maps.LatLngBounds(
-                        new kakao.maps.LatLng(resultJsonData.result.boundary.top, resultJsonData.result.boundary.left),
-                        new kakao.maps.LatLng(resultJsonData.result.boundary.bottom, resultJsonData.result.boundary.right)
-                    );
-                    map.panToBounds(boundary);
+    $("#btn_select2")
+        .click(
+            function () {
+                function searchPubTransPathAJAX() {
+                    var xhr = new XMLHttpRequest();
+                    //ODsay apiKey 입력
+                    var url = "https://api.odsay.com/v1/api/searchPubTransPath?apiKey=VYYJtQrZq5ere3U%2BvOoPhLmqgvRTrFzcpLKrRaKvpcQ&SX="+sx1+"&SY="+sy1+"&EX="+ex1+"&EY="+ey1+"&apiKey={YOUR_API_KEY}";
+                    xhr.open("GET", url, true);
+                    xhr.send();
+                    xhr.onreadystatechange = function() {
+                        if (xhr.readyState == 4 && xhr.status == 200) {
+                            console.log( JSON.parse(xhr.responseText) ); // <- xhr.responseText 로 결과를 가져올 수 있음
+                            //노선그래픽 데이터 호출
+                            callMapObjApiAJAX((JSON.parse(xhr.responseText))["result"]["path"][0].info.mapObj);
+                        }
+                    }
                 }
-            }
-        }
-    }
+
+                //길찾기 API 호출
+                searchPubTransPathAJAX();
+
+                function callMapObjApiAJAX(mabObj){
+                    var xhr = new XMLHttpRequest();
+                    //ODsay apiKey 입력
+                    var url = "https://api.odsay.com/v1/api/loadLane?apiKey=VYYJtQrZq5ere3U%2BvOoPhLmqgvRTrFzcpLKrRaKvpcQ&mapObject=0:0@"+mabObj+"&apiKey={YOUR_API_KEY}";
+                    xhr.open("GET", url, true);
+                    xhr.send();
+                    xhr.onreadystatechange = function() {
+                        if (xhr.readyState == 4 && xhr.status == 200) {
+                            var resultJsonData = JSON.parse(xhr.responseText);
+                            drawKakaoMarker(sx1,sy1);					// 출발지 마커 표시
+                            drawKakaoMarker(ex1,ey1);					// 도착지 마커 표시
+                            drawKakaoPolyLine(resultJsonData);		// 노선그래픽데이터 지도위 표시
+                            // boundary 데이터가 있을경우, 해당 boundary로 지도이동
+                            if(resultJsonData.result.boundary){
+                                var boundary = new kakao.maps.LatLngBounds(
+                                    new kakao.maps.LatLng(resultJsonData.result.boundary.top, resultJsonData.result.boundary.left),
+                                    new kakao.maps.LatLng(resultJsonData.result.boundary.bottom, resultJsonData.result.boundary.right)
+                                );
+                                map.panToBounds(boundary);
+                            }
+                        }
+                    }
+                }
+
+            });
 
     // 지도위 마커 표시해주는 함수
     function drawKakaoMarker(x,y){
         var marker = new kakao.maps.Marker({
             position: new kakao.maps.LatLng(y, x),
-            map: map
+            map : map
         });
     }
 

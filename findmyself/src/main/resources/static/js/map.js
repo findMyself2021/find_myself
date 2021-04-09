a578a6d925d610845918adf15c0c3c1860c6c1e8
daeun <sung980524@naver.com>
2021-04-09 20:24:22 +0900

    마커 및 경로 색 수정

--- findmyself/src/main/resources/static/js/map.js
+++ findmyself/src/main/resources/static/js/map.js
@@ -158,12 +158,6 @@ function Draw_HangJungDong(h_code, addr){
 
 var polygons = [];
 
-// 지도에 영역데이터를 폴리곤으로 표시합니다
-for (var i = 0, len = areas.length; i < len; i++) {
-    displayArea(areas[i]);
-}
-
-
 // 특정 행정동 색칠
 function displayHangJungDong(coordinates, name,code,dest){
     var path = [];
